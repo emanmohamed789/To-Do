8fb8e9803e27965b0a8b5985afc373c36c90393c
BAB AL SAFA <emanmohamedmahmoud789@gmail.com>
2024-02-28 21:50:25 +0200

    settings without shared preferences

--- app/src/main/java/com/example/todoapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/SettingsFragment.kt
@@ -7,15 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentSettingsBinding
 import java.util.Locale
 
-@Suppress("DEPRECATION")
 class SettingsFragment : Fragment() {
 
+    //private var settingsSP: SharedPreferences? = null
     private lateinit var binding: FragmentSettingsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,55 +30,130 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        settingsSP = activity?.getSharedPreferences("com.example.todoapp.settings",Context.MODE_PRIVATE)
+//
+//        checkAndSetSettingsPreferences()
 
-        val languages = resources.getStringArray(R.array.languages)
-        val languageAdapter =
-            ArrayAdapter(requireContext(), R.layout.drop_down_item, languages)
-        binding.autoCompleteTVLanguages.setAdapter(languageAdapter)
+        setupExposedDropDownMenus()
 
-        val modes = resources.getStringArray(R.array.modes)
-        val modeAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, modes)
+        binding.autoCompleteTVModes.doOnTextChanged { text, _, _, _ ->
+            activeDarkMode(
+                when (text.toString()) {
+                    resources.getString(R.string.dark) -> {
+                        //settingsSP!!.edit().putBoolean("MODE",true).apply()
+                        true
+                    }
+
+                    else -> {
+//                        settingsSP!!.edit().putBoolean("MODE",false).apply()
+                        false
+                    }
+                }
+            )
+        }
+
+        binding.autoCompleteTVLanguages.doOnTextChanged { text, _, _, _ ->
+            activateArabicLang(
+                when (text.toString()) {
+                    resources.getString(R.string.arabic) -> {
+                        true
+                    }
+
+                    else -> {
+                        false
+                    }
+                }
+            )
+        }
+    }
+
+//    private fun checkAndSetSettingsPreferences() {
+//        activeDarkMode(settingsSP?.getBoolean("com.example.todoapp.settings",false) ?: false)
+//    }
+
+    private fun activateArabicLang(activate: Boolean) {
+        setLocale(if (activate) "ar" else "en")
+        activity?.let { ActivityCompat.recreate(it) }
+    }
+
+
+    private fun activeDarkMode(activate: Boolean) {
+        //activity?.let { ActivityCompat.recreate(it)}
+        AppCompatDelegate.setDefaultNightMode(
+            if (activate) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
+        activity?.let { ActivityCompat.recreate(it) }
+    }
+
+    private fun populateMenusWithOptions() {
+        binding.autoCompleteTVLanguages.setText(
+            when (isArabicLangActive()) {
+                true -> resources.getString(R.string.arabic)
+                false -> resources.getString(R.string.english)
+            }, false
+        )
+
+        binding.autoCompleteTVModes.setText(
+            when (isDarkModeActive()) {
+                true -> resources.getString(R.string.dark)
+                false -> resources.getString(R.string.light)
+            }, false
+        )
+    }
+
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        activity?.let {
+            @Suppress("DEPRECATION")
+            requireActivity().baseContext.resources.updateConfiguration(
+                config,
+                requireActivity().baseContext.resources.displayMetrics
+            )
+        }
+    }
+
+    private fun isDarkModeActive(): Boolean {
+
+        return when (
+            context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            else -> false
+        }
+    }
+
+
+    //RequiresApi(Build.VERSION_CODES.N)
+    private fun isArabicLangActive(): Boolean {
+        return when (context?.resources?.configuration?.locales!![0].language) {
+            "ar" -> true
+            else -> false
+        }
+    }
+
+
+    //RequiresApi(Build.VERSION_CODES.N)
+    private fun setupExposedDropDownMenus() {
+
+        val langItems = listOf(
+            resources.getString(R.string.english),
+            resources.getString(R.string.arabic)
+        )
+        val langAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, langItems)
+        binding.autoCompleteTVLanguages.setAdapter(langAdapter)
+
+        val modeItems = listOf(
+            resources.getString(R.string.light),
+            resources.getString(R.string.dark)
+        )
+        val modeAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, modeItems)
         binding.autoCompleteTVModes.setAdapter(modeAdapter)
 
-        binding.autoCompleteTVLanguages.setOnItemClickListener { _, _, position, _ ->
-            val selectedLanguage = languages[position]
-            changeLanguage(selectedLanguage)
-        }
 
-        binding.autoCompleteTVModes.setOnItemClickListener { _, _, position, _ ->
-            val selectedMode = modes[position]
-            changeMode(selectedMode)
-        }
-    }
+        populateMenusWithOptions()
 
-    private fun changeLanguage(selectedLanguage: String?) {
-        when (selectedLanguage) {
-            "Arabic", "العربيه" -> setLanguage("ar")
-            "English", "الانجليزيه" -> setLanguage("en")
-        }
-    }
-
-    private fun changeMode(mode: String) {
-        when (mode) {
-            "Light", "الوضع الضوء" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            "Dark", "الوضع المظلم" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
-    }
-
-    private fun setLanguage(langCode: String) {
-        val resources = resources
-        val configuration = Configuration(resources.configuration)
-        val locale = Locale(langCode)
-        configuration.setLocale(locale)
-
-        val displayMetrics = resources.displayMetrics
-
-        resources.updateConfiguration(configuration, displayMetrics)
-
-        val editor = requireActivity().getPreferences(0).edit()
-        editor.putString("selected_language", langCode)
-        editor.apply()
-
-        requireActivity().recreate()
     }
 }
+
